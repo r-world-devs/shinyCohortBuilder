e49befd9b1f59589537ac475af818fc3d9a6cb78
Krystian Igras <krystian.igras@contractors.roche.com>
2026-02-25 10:20:48 +0000

    Update docs.

--- R/source.R
+++ R/source.R
@@ -47,18 +47,19 @@ autofilter.default <- function(source, ...) {
 
 #' Create target filter position in UI panel
 #'
-#' @param Source Source object.
+#' @param source Source object.
 #' @param step_id Id of the step that filter belong to.
-#' @param name filter Filter object.
+#' @param filter Filter object.
+#' @param ns Namespace function.
 #' @param ... Extra arguments passed to related method.
 #' @name filter-position
 #' @export
-.filter_position <- function(source, step_id, filter, ...) {
+.filter_position <- function(source, step_id, filter, ns, ...) {
   UseMethod(".filter_position", source)
 }
 
 #' @rdname filter-position
 #' @export
-.filter_position.default <- function(source, step_id, filter, ...) {
+.filter_position.default <- function(source, step_id, filter, ns, ...) {
   return(step_id)
 }
--- R/ui_utils.R
+++ R/ui_utils.R
@@ -122,6 +122,7 @@ scb_chart_palette <- list(
 #'
 #' @param session Shiny session object.
 #' @param cohort_id Id of the cohort.
+#' @param step_id Id of the step to check. When NULL (default) all the steps are checked for changes.
 #'
 #' @export
 cb_changed <- function(session, cohort_id, step_id = NULL) {
